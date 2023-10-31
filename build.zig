417947c07aebc19040cc5dc4c63217964ac8d79f
Tristan Ross <tristan.ross@midstall.com>
2023-10-30 23:34:28 -0700

    feat: support external backends

--- build.zig
+++ build.zig
@@ -1,7 +1,19 @@
 const std = @import("std");
 const metap = @import("metaplus").@"meta+";
 
-pub const BackendType = metap.enums.fromDecls(@import("src/phantom/scene/backends.zig"));
+pub const BackendType = metap.enums.fromDecls(struct {
+    const shouldImport = blk: {
+        for (@import("root").dependencies.root_deps) |dep| {
+            if (std.mem.eql(u8, dep[0], "phantom")) break :blk true;
+        }
+        break :blk false;
+    };
+
+    const root = if (shouldImport) @import("root.@build") else struct {};
+
+    pub usingnamespace @import("src/phantom/scene/backends.zig");
+    pub usingnamespace if (@hasDecl(root, "phantom")) if (@hasDecl(root.phantom, "backends")) root.phantom.backends else struct {} else struct {};
+});
 
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
--- src/phantom/scene/backends.zig
+++ src/phantom/scene/backends.zig
@@ -1,1 +1,4 @@
+const root = @import("root");
+
 pub const headless = @import("backends/headless.zig");
+pub usingnamespace if (@hasDecl(root, "phantom")) if (@hasDecl(root.phantom, "backends")) root.phantom.backends else struct {} else struct {};
