9eea2a89d3ffb76ff4623aa70f28f02177b22ba3
Tristan Ross <tristan.ross@midstall.com>
2023-11-02 13:26:23 -0700

    feat(scene): add base node types mixing with backend

--- src/phantom/scene.zig
+++ src/phantom/scene.zig
@@ -1,13 +1,20 @@
 const metaplus = @import("meta+");
 
+const base = struct {
+    pub const Node = @import("scene/node.zig");
+    pub const NodeTree = @import("scene/node-tree.zig");
+    pub const NodeFlex = @import("scene/node-flex.zig");
+};
+
 pub const Base = @import("scene/base.zig");
-pub const Node = @import("scene/node.zig");
-pub const NodeTree = @import("scene/node-tree.zig");
-pub const NodeFlex = @import("scene/node-flex.zig");
+pub usingnamespace base;
 
 pub const backends = @import("scene/backends.zig");
 pub const BackendType = metaplus.enums.fromDecls(backends);
 
 pub fn Backend(comptime T: BackendType) type {
-    return @field(backends, @tagName(T));
+    return struct {
+        pub usingnamespace @field(backends, @tagName(T));
+        pub usingnamespace base;
+    };
 }
