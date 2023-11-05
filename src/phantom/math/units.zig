a55c18ac8f7c200493442dc241285dc4c94768ea
Tristan Ross <tristan.ross@midstall.com>
2023-11-05 10:37:45 -0800

    feat(units): simplify

--- src/phantom/math/units.zig
+++ src/phantom/math/units.zig
@@ -2,17 +2,17 @@ const vizops = @import("vizops");
 const Node = @import("../scene/node.zig");
 
 pub fn rel(frameInfo: Node.FrameInfo, value: vizops.vector.Float32Vector2) vizops.vector.UsizeVector2 {
-    return value.mul(frameInfo.scale).mul(frameInfo.size.res.cast(f32)).div(100.0).cast(usize);
+    return value.mul(frameInfo.scale).mul(frameInfo.size.res.cast(f32)).div(@as(f32, 100.0)).cast(usize);
 }
 
 pub fn inches(frameInfo: Node.FrameInfo, value: vizops.vector.Float32Vector2) vizops.vector.UsizeVector2 {
-    const physicalInches = frameInfo.size.phys.div(vizops.vector.Float32Vector2.init(25.4));
+    const physicalInches = frameInfo.size.phys.div(@as(f32, 25.4));
     const dpi = frameInfo.size.res.cast(f32).div(physicalInches).mul(frameInfo.scale);
     return dpi.mul(value).cast(usize);
 }
 
 pub fn cm(frameInfo: Node.FrameInfo, value: vizops.vector.Float32Vector2) vizops.vector.UsizeVector2 {
-    const physicalCm = frameInfo.size.phys.div(vizops.vector.Float32Vector2.init(10.0));
+    const physicalCm = frameInfo.size.phys.div(@as(f32, 10.0));
     const dpcm = frameInfo.size.res.cast(f32).div(physicalCm).mul(frameInfo.scale);
     return dpcm.mul(value).cast(usize);
 }
