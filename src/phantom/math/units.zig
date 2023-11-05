655d9a9a2e2079369d79d9006d4c8d22d888ad09
Tristan Ross <tristan.ross@midstall.com>
2023-11-05 10:33:34 -0800

    feat(units): use new vizops function

--- src/phantom/math/units.zig
+++ src/phantom/math/units.zig
@@ -1,45 +1,22 @@
 const vizops = @import("vizops");
 const Node = @import("../scene/node.zig");
-const UsizeVector = vizops.vector.UsizeVector2;
 
-pub fn rel(frameInfo: Node.FrameInfo, value: vizops.vector.Float32Vector2) UsizeVector {
-    return UsizeVector.init([_]usize{
-        @intFromFloat(value.value[0] * frameInfo.scale.value[0] * @as(f32, @floatFromInt(frameInfo.size.res.value[0])) / 100.0),
-        @intFromFloat(value.value[1] * frameInfo.scale.value[1] * @as(f32, @floatFromInt(frameInfo.size.res.value[1])) / 100.0),
-    });
+pub fn rel(frameInfo: Node.FrameInfo, value: vizops.vector.Float32Vector2) vizops.vector.UsizeVector2 {
+    return value.mul(frameInfo.scale).mul(frameInfo.size.res.cast(f32)).div(100.0).cast(usize);
 }
 
-pub fn inches(frameInfo: Node.FrameInfo, value: vizops.vector.Float32Vector2) UsizeVector {
-    const physicalInches = frameInfo.size.phys.div(vizops.vector.Float32Vector2.init(.{25.4} ** 2));
-    const dpi = vizops.vector.Float32Vector2.init(.{
-        @as(f32, @floatFromInt(frameInfo.size.res.value[0])) / physicalInches.value[0],
-        @as(f32, @floatFromInt(frameInfo.size.res.value[1])) / physicalInches.value[1],
-    }).mul(frameInfo.scale);
-    return UsizeVector.init([_]usize{
-        @intFromFloat(dpi.value[0] * value.value[0]),
-        @intFromFloat(dpi.value[1] * value.value[1]),
-    });
+pub fn inches(frameInfo: Node.FrameInfo, value: vizops.vector.Float32Vector2) vizops.vector.UsizeVector2 {
+    const physicalInches = frameInfo.size.phys.div(vizops.vector.Float32Vector2.init(25.4));
+    const dpi = frameInfo.size.res.cast(f32).div(physicalInches).mul(frameInfo.scale);
+    return dpi.mul(value).cast(usize);
 }
 
-pub fn cm(frameInfo: Node.FrameInfo, value: vizops.vector.Float32Vector2) UsizeVector {
-    const physicalCm = frameInfo.size.phys.div(vizops.vector.Float32Vector2.init(.{10.0} ** 2));
-    const dpcm = vizops.vector.Float32Vector2.init(.{
-        @as(f32, @floatFromInt(frameInfo.size.res.value[0])) / physicalCm.value[0],
-        @as(f32, @floatFromInt(frameInfo.size.res.value[1])) / physicalCm.value[1],
-    }).mul(frameInfo.scale);
-    return UsizeVector.init([_]usize{
-        @intFromFloat(dpcm.value[0] * value.value[0]),
-        @intFromFloat(dpcm.value[1] * value.value[1]),
-    });
+pub fn cm(frameInfo: Node.FrameInfo, value: vizops.vector.Float32Vector2) vizops.vector.UsizeVector2 {
+    const physicalCm = frameInfo.size.phys.div(vizops.vector.Float32Vector2.init(10.0));
+    const dpcm = frameInfo.size.res.cast(f32).div(physicalCm).mul(frameInfo.scale);
+    return dpcm.mul(value).cast(usize);
 }
 
-pub fn mm(frameInfo: Node.FrameInfo, value: vizops.vector.Float32Vector2) UsizeVector {
-    const dpmm = vizops.vector.Float32Vector2.init(.{
-        @as(f32, @floatFromInt(frameInfo.size.res.value[0])) / frameInfo.size.phys.value[0],
-        @as(f32, @floatFromInt(frameInfo.size.res.value[1])) / frameInfo.size.phys.value[1],
-    }).mul(frameInfo.scale);
-    return UsizeVector.init([_]usize{
-        @intFromFloat(dpmm.value[0] * value.value[0]),
-        @intFromFloat(dpmm.value[1] * value.value[1]),
-    });
+pub fn mm(frameInfo: Node.FrameInfo, value: vizops.vector.Float32Vector2) vizops.vector.UsizeVector2 {
+    return frameInfo.size.res.cast(f32).div(frameInfo.size.phys).mul(frameInfo.scale).mul(value).cast(usize);
 }
