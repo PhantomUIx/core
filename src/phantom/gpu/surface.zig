344fd7d53b5a5ef217100c6809ab95ac8f8cc8f8
Tristan Ross <tristan.ross@midstall.com>
2023-11-07 00:15:03 -0800

    feat(gpu): add blt and update info to surface

--- src/phantom/gpu/surface.zig
+++ src/phantom/gpu/surface.zig
@@ -1,14 +1,19 @@
 const std = @import("std");
 const vizops = @import("vizops");
+const fb = @import("../painting/fb.zig");
 const Surface = @This();
+
+pub const Blt = enum { from, to };
 
 pub const Info = struct {
     format: u32,
-    size: vizops.vector.UsizeVector2 = vizops.vector.UsizeVector2.zero(),
+    size: vizops.vector.UsizeVector2,
 };
 
 pub const VTable = struct {
     info: *const fn (*anyopaque) anyerror!Info,
+    updateInfo: *const fn (*anyopaque, Info) anyerror!void,
+    blt: *const fn (*anyopaque, Blt, *fb.Base) anyerror!void,
     dupe: *const fn (*anyopaque) anyerror!*Surface,
     deinit: ?*const fn (*anyopaque) void = null,
 };
@@ -18,6 +23,14 @@ ptr: *anyopaque,
 
 pub inline fn info(self: *Surface) !Info {
     return self.vtable.info(self.ptr);
+}
+
+pub inline fn updateInfo(self: *Surface, i: Info) !void {
+    return self.vtable.updateInfo(self.ptr, i);
+}
+
+pub inline fn blt(self: *Surface, mode: Blt, op: *fb.Base) !void {
+    return self.vtable.blt(self.ptr, mode, op);
 }
 
 pub inline fn dupe(self: *Surface) !*Surface {
