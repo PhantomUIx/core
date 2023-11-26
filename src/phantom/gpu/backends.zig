348fc788047eb55ea3098b7d2c19f55bfa0f5283
Tristan Ross <tristan.ross@midstall.com>
2023-11-26 15:32:42 -0800

    fix: typo

--- src/phantom/gpu/backends.zig
+++ src/phantom/gpu/backends.zig
@@ -1,5 +1,5 @@
 const root = @import("root");
 const imports = @import("../importer.zig").imported;
 
-pub usingnamespace if (@hasDecl(root, "phantomOptions")) if (@hasDecl(root.phantom, "gpuBackends")) root.phantomOptions.gpuBackends else struct {} else struct {};
+pub usingnamespace if (@hasDecl(root, "phantomOptions")) if (@hasDecl(root.phantomOptions, "gpuBackends")) root.phantomOptions.gpuBackends else struct {} else struct {};
 pub usingnamespace if (@hasDecl(imports, "gpu")) if (@hasDecl(imports.gpu, "backends")) imports.gpu.backends else struct {} else struct {};
--- src/phantom/scene/backends.zig
+++ src/phantom/scene/backends.zig
@@ -4,5 +4,5 @@ const imports = @import("../importer.zig").imported;
 pub const headless = @import("backends/headless.zig");
 pub const fb = @import("backends/fb.zig");
 
-pub usingnamespace if (@hasDecl(root, "phantomOptions")) if (@hasDecl(root.phantom, "sceneBackends")) root.phantomOptions.sceneBackends else struct {} else struct {};
+pub usingnamespace if (@hasDecl(root, "phantomOptions")) if (@hasDecl(root.phantomOptions, "sceneBackends")) root.phantomOptions.sceneBackends else struct {} else struct {};
 pub usingnamespace if (@hasDecl(imports, "scene")) if (@hasDecl(imports.scene, "backends")) imports.scene.backends else struct {} else struct {};
