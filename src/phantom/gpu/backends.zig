12e201350459b8be06a35d108651237a506d8706
Tristan Ross <tristan.ross@midstall.com>
2023-11-26 13:18:37 -0800

    fix: build issues

--- src/phantom/gpu/backends.zig
+++ src/phantom/gpu/backends.zig
@@ -1,5 +1,5 @@
 const root = @import("root");
-const imports = if (@hasDecl(root, "dependencies")) struct {} else @import("phantom.imports").import(@import("../../phantom.zig"));
+const imports = @import("../importer.zig").imported;
 
 pub usingnamespace if (@hasDecl(root, "phantomOptions")) if (@hasDecl(root.phantom, "gpuBackends")) root.phantomOptions.gpuBackends else struct {} else struct {};
 pub usingnamespace if (@hasDecl(imports, "gpu")) if (@hasDecl(imports.gpu, "backends")) imports.gpu.backends else struct {} else struct {};
