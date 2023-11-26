1fc8328b5353e3f34e6a34acc43cd0d14131f838
Tristan Ross <tristan.ross@midstall.com>
2023-11-26 15:29:20 -0800

    fix: typo

--- src/phantom/display/backends.zig
+++ src/phantom/display/backends.zig
@@ -3,5 +3,5 @@ const imports = @import("../importer.zig").imported;
 
 pub const headless = @import("backends/headless.zig");
 
-pub usingnamespace if (@hasDecl(root, "phantomOptions")) if (@hasDecl(root.phantom, "displayBackends")) root.phantomOptions.displayBackends else struct {} else struct {};
+pub usingnamespace if (@hasDecl(root, "phantomOptions")) if (@hasDecl(root.phantomOptions, "displayBackends")) root.phantomOptions.displayBackends else struct {} else struct {};
 pub usingnamespace if (@hasDecl(imports, "display")) if (@hasDecl(imports.display, "backends")) imports.display.backends else struct {} else struct {};
