c7aa8961053e12ad1bf6120285a539fce632acfd
Tristan Ross <tristan.ross@midstall.com>
2023-12-31 10:30:14 -0800

    refactor(node-rect): implement #2

--- src/phantom/scene/backends/fb.zig
+++ src/phantom/scene/backends/fb.zig
@@ -1,4 +1,4 @@
 pub const Scene = @import("fb/scene.zig");
 pub const NodeArc = @import("fb/node-arc.zig");
-pub const NodeRect = @import("fb/node-rect.zig");
+pub const NodeRect = @import("fb/node-rect.zig").NodeRect;
 pub const NodeFrameBuffer = @import("fb/node-fb.zig");
