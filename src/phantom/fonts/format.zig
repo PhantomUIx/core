949bba0bd6fef66f08937324137a02bc790450e8
Tristan Ross <tristan.ross@midstall.com>
2024-02-27 20:50:58 -0800

    feat(fonts): add font load options

--- src/phantom/fonts/format.zig
+++ src/phantom/fonts/format.zig
@@ -1,15 +1,23 @@
 const std = @import("std");
 const metap = @import("meta+");
 const Font = @import("font.zig");
+const vizops = @import("vizops");
 const Self = @This();
 
+pub const LoadOptions = struct {
+    foregroundColor: vizops.color.Any,
+    backgroundColor: vizops.color.Any,
+    colorspace: std.meta.DeclEnum(vizops.color.types),
+    colorFormat: vizops.color.fourcc.Value,
+};
+
 const BaseVTable = struct {
-    loadBuffer: *const fn (*anyopaque, []const u8) anyerror!*Font,
+    loadBuffer: *const fn (*anyopaque, []const u8, LoadOptions) anyerror!*Font,
     deinit: ?*const fn (*anyopaque) void = null,
 };
 
 const FsVTable = struct {
-    loadFile: ?*const fn (*anyopaque, std.fs.File) anyerror!*Font = null,
+    loadFile: ?*const fn (*anyopaque, std.fs.File, LoadOptions) anyerror!*Font = null,
 };
 
 pub const VTable = metap.structs.fields.mix(BaseVTable, if (@hasDecl(std.os.system, "fd_t")) FsVTable else struct {});
@@ -17,13 +25,13 @@ pub const VTable = metap.structs.fields.mix(BaseVTable, if (@hasDecl(std.os.syst
 ptr: *anyopaque,
 vtable: *const VTable,
 
-pub inline fn loadBuffer(self: *Self, buff: []const u8) !*Font {
-    return self.vtable.loadBuffer(self.ptr, buff);
+pub inline fn loadBuffer(self: *Self, buff: []const u8, options: LoadOptions) !*Font {
+    return self.vtable.loadBuffer(self.ptr, buff, options);
 }
 
-pub inline fn loadFile(self: *Self, file: std.fs.File) !*Font {
+pub inline fn loadFile(self: *Self, file: std.fs.File, options: LoadOptions) !*Font {
     if (@hasDecl(std.os.system, "fd_t")) {
-        if (self.vtable.loadFile) |f| return f(self.ptr, file);
+        if (self.vtable.loadFile) |f| return f(self.ptr, file, options);
         return error.NotImplemented;
     }
     return error.NotSupported;
