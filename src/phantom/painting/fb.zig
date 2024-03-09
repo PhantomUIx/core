7693599fbe60d82493c54ee614cded565e27ac37
Tristan Ross <tristan.ross@midstall.com>
2024-03-08 21:03:27 -0800

    feat: add memory frame buffer

--- src/phantom/painting/fb.zig
+++ src/phantom/painting/fb.zig
@@ -1,3 +1,4 @@
 pub const Base = @import("fb/base.zig");
 pub const AllocatedFrameBuffer = @import("fb/alloc.zig");
+pub const MemoryFrameBuffer = @import("fb/mem.zig");
 pub const FileDescriptorFrameBuffer = @import("fb/fd.zig");
